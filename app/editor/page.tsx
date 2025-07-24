67288ab676417c88b1f23fef1667a33665b76867
heliacer <heliacer@gmx.ch>
2025-07-24 15:19:19 +0200

    dockview test

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,28 +1,49 @@
 'use client'
 
+import { DockviewReact, DockviewReadyEvent, IDockviewPanelProps, themeDark } from "dockview-react"
+// Needs granular cleaning
+import './styles/bloat.css'
 import { Folder, LogOut } from "lucide-react"
-import { signOut } from "next-auth/react"
 import Link from "next/link"
-import EarlyAccessLogo from "../ui/ea-logo"
+import { signOut } from "next-auth/react"
+
+const Default = (props: IDockviewPanelProps) => {
+  return <div>{props.api.title}</div>
+}
+
+const components = {
+  default: Default
+}
 
 export default function Editor() {
+  function mount(event: DockviewReadyEvent) {
+    // Testing
+    event.api.addPanel({ id: crypto.randomUUID(), component: 'default' })
+    event.api.addPanel({ id: crypto.randomUUID(), component: 'default' })
+    event.api.addPanel({ id: crypto.randomUUID(), component: 'default' })
+    const a = event.api.addPanel({ id: crypto.randomUUID(), component: 'default', position: { direction: 'right' } })
+    event.api.addPanel({ id: crypto.randomUUID(), component: 'default' })
+    const b = event.api.addPanel({ id: crypto.randomUUID(), component: 'default', position: { referencePanel: a, direction: 'below' } })
+    event.api.addPanel({ id: crypto.randomUUID(), component: 'default', position: { referencePanel: b, direction: 'right' } })
+  }
+
   return (
-    <main className="flex justify-center items-center min-h-screen">
-      <div className="w-xl flex flex-wrap-reverse items-end justify-between  gap-6 mx-10">
-        <div className="flex min-h-52 flex-col gap-4">
-          <p className="text-xl">Editor</p>
-          <p>Psst!</p>
-          <Link className='flex items-center gap-2' href='/mystuff'>
-            <Folder size={18} />
-            <p>Go to my stuff</p>
-          </Link>
-          <button className="cursor-pointer flex items-center gap-2" onClick={() => signOut({ callbackUrl: '/' })}>
-            <LogOut size={18} />
-            <p>Sign Out</p>
-          </button>
-        </div>
-        <EarlyAccessLogo size={90} />
-      </div>
+    <main className="h-screen flex flex-col">
+      <nav className="h-10 flex gap-4 items-center">
+        <p>Nav</p>
+        <Link className='flex items-center gap-2' href='/mystuff'>
+          <Folder size={18} />
+          <p>My stuff</p>
+        </Link>
+        <button className="cursor-pointer flex items-center gap-2" onClick={() => signOut({ callbackUrl: '/' })}>
+          <LogOut size={18} />
+          <p>Sign Out</p>
+        </button>
+      </nav>
+      <DockviewReact theme={themeDark} className="w-full h-full" onReady={mount} components={components} />
+      <footer className="h-6 flex items-center">
+        <p>Footer</p>
+      </footer>
     </main>
   )
 }
