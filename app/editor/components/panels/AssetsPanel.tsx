2cfab0d605be4b7ed626cb60f27b4a29407671fe
heliacer <heliacer@gmx.ch>
2025-07-27 22:03:03 +0200

    manifest testing

--- app/editor/components/panels/AssetsPanel.tsx
+++ app/editor/components/panels/AssetsPanel.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+
 export default function AssetsPanel() {
   /**
    * Asset Logic
@@ -33,19 +35,33 @@ export default function AssetsPanel() {
     Storage Options
     - Setting (default): cache cloud / url objects locally, improve loading time
   */
+  const [manifestData, setManifestData] = useState<any>(null)
+
+  useEffect(() => {
+    fetch('https://graplet.github.io/assetlib/manifest.json')
+    .then(response => response.json())
+    .then(data => setManifestData(data))
+    .catch(error => console.error('Error fetching manifest:', error))
+  }, [])
 
   return (
     <>
-    <div className="flex">
-      <input 
-        type="text"
-        className="w-full"
-        placeholder="type Asset URL or choose from Library"
-      />
-      <button className="text-nowrap">
-        Add Empty
-      </button>
-    </div>
+      <div className="flex px-1.5 py-2 gap-1.5">
+        <button className="text-nowrap cursor-pointer rounded px-1.5 bg-zinc-800 border border-zinc-700">
+          Add empty
+        </button>
+        <input 
+          type="text"
+          className="w-full focus:outline-none rounded px-1.5 border border-zinc-700"
+          placeholder="Type Asset URL or choose from Library"
+        />
+      </div>
+      <div className="px-1.5">
+        {manifestData &&
+        <pre className="text-xs bg-zinc-900 p-2 rounded overflow-auto max-h-80">
+          {JSON.stringify(manifestData, null, 2)}
+        </pre>}
+      </div>
     </>
   )
 }
