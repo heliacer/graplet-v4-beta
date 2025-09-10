cc09ee1cf0ddc7418e98d06002b290857ad7cae1
heliacer <heliacer@gmx.ch>
2025-09-10 18:05:35 +0200

    add ifReturn override

--- app/editor/lib/blockly/config.ts
+++ app/editor/lib/blockly/config.ts
@@ -1,14 +1,13 @@
 import {
-  Block,
   blockRendering,
   common,
   ContextMenuItems,
   Extensions,
-  FieldDropdown,
   registry,
   Scrollbar,
   ToolboxCategory,
-  VerticalFlyout
+  VerticalFlyout,
+  Blocks
 } from 'blockly'
 import {
   ContinuousCategory,
@@ -20,54 +19,11 @@ import {
 import { registerFieldAngle } from '@blockly/field-angle'
 import { GrapletRenderer } from './renderer'
 import { definitions } from './blocks'
-
-interface DivisiblebyBlock extends Block {
-  updateShape_: (divisorInput: boolean) => void
-}
-
-const IS_DIVISIBLEBY_MUTATOR_MIXIN = {
-  saveExtraState: function (this: Block) {
-    return {
-      divisorInput: this.getFieldValue('PROPERTY') === 'DIVISIBLE_BY'
-    }
-  },
-
-  loadExtraState: function (
-    this: DivisiblebyBlock,
-    state: { divisorInput: boolean }
-  ) {
-    this.updateShape_(state['divisorInput'])
-  },
-
-  updateShape_: function (this: DivisiblebyBlock, divisorInput: boolean) {
-    const input = this.getInput('DIVISOR')
-
-    if (divisorInput) {
-      if (!input) {
-        const newInput = this.appendValueInput('DIVISOR').setCheck('Number')
-        newInput.connection?.setShadowState({
-          type: 'math_number',
-          fields: { NUM: 2 }
-        })
-      }
-    } else if (input) {
-      input.connection?.setShadowState(null)
-      this.removeInput('DIVISOR')
-    }
-  }
-}
-
-const IS_DIVISIBLE_MUTATOR_EXTENSION = function (this: DivisiblebyBlock) {
-  this.getField('PROPERTY')!.setValidator(function (
-    this: FieldDropdown,
-    option: string
-  ) {
-    const divisorInput = option === 'DIVISIBLE_BY'
-    const sourceBlock = this.getSourceBlock() as DivisiblebyBlock
-    sourceBlock.updateShape_(divisorInput)
-    return undefined
-  })
-}
+import {
+  isDivisibleMutatorMixin,
+  isDivisibleMutatorExtension
+} from './mutators/isDivisible'
+import { PROCEDURES_IFRETURN } from './mutators/ifReturn'
 
 // TODO: Override ifreturn mutator mixins
 
@@ -75,9 +31,11 @@ export function initializeBlockly() {
   Extensions.unregister('math_is_divisibleby_mutator')
   Extensions.registerMutator(
     'math_is_divisibleby_mutator',
-    IS_DIVISIBLEBY_MUTATOR_MIXIN,
-    IS_DIVISIBLE_MUTATOR_EXTENSION
+    isDivisibleMutatorMixin,
+    isDivisibleMutatorExtension
   )
+
+  Blocks['procedures_ifreturn'] = PROCEDURES_IFRETURN
 
   Scrollbar.scrollbarThickness = 10
   registerFieldAngle()
---
+++ app/editor/lib/blockly/mutators/isDivisible.ts
@@ -0,0 +1,53 @@
+import { FieldDropdown, Block } from 'blockly'
+
+/**
+ * @override isDivisibleBy mutator
+ */
+
+interface DivisiblebyBlock extends Block {
+  updateShape_: (divisorInput: boolean) => void
+}
+
+export const isDivisibleMutatorMixin = {
+  saveExtraState: function (this: Block) {
+    return {
+      divisorInput: this.getFieldValue('PROPERTY') === 'DIVISIBLE_BY'
+    }
+  },
+
+  loadExtraState: function (
+    this: DivisiblebyBlock,
+    state: { divisorInput: boolean }
+  ) {
+    this.updateShape_(state['divisorInput'])
+  },
+
+  updateShape_: function (this: DivisiblebyBlock, divisorInput: boolean) {
+    const input = this.getInput('DIVISOR')
+
+    if (divisorInput) {
+      if (!input) {
+        const newInput = this.appendValueInput('DIVISOR').setCheck('Number')
+        newInput.connection?.setShadowState({
+          type: 'math_number',
+          fields: { NUM: 2 }
+        })
+      }
+    } else if (input) {
+      input.connection?.setShadowState(null)
+      this.removeInput('DIVISOR')
+    }
+  }
+}
+
+export const isDivisibleMutatorExtension = function (this: DivisiblebyBlock) {
+  this.getField('PROPERTY')!.setValidator(function (
+    this: FieldDropdown,
+    option: string
+  ) {
+    const divisorInput = option === 'DIVISIBLE_BY'
+    const sourceBlock = this.getSourceBlock() as DivisiblebyBlock
+    sourceBlock.updateShape_(divisorInput)
+    return undefined
+  })
+}
