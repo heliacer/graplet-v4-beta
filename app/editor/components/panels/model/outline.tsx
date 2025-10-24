e66c3279fe0cc0a6064732cc16863edf8b9e0078
heliacer <heliacer@gmx.ch>
2025-10-24 19:47:53 +0200

    finnally add tree :> outline

--- app/editor/components/panels/model/outline.tsx
+++ app/editor/components/panels/model/outline.tsx
@@ -1,52 +1,67 @@
-import { useEditor } from '@/app/editor/lib/EditorContext'
-import { Object3D } from 'three'
+import { useEditor } from "@/app/editor/lib/EditorContext"
+import { hotkeysCoreFeature, selectionFeature, syncDataLoaderFeature } from "@headless-tree/core"
+import { useTree } from "@headless-tree/react"
+import clsx from "clsx"
+import { useEffect } from "react"
+import { Object3D } from "three"
 
-interface TreeItem {
-  name: string
-  children: TreeItem[]
-}
+export default function Ouline() {
+  const { scene, objectVersion, currentObject } = useEditor()
+  if (!currentObject) return
 
-function getObjectTree(object: Object3D): TreeItem {
-  const children = []
-  for (const child of object.children) {
-    children.push(getObjectTree(child))
-  }
-  return {
-    name: object.name,
-    children
-  }
-}
+  const tree = useTree<Object3D>({
+    rootItemId: currentObject.id.toString(),
+    getItemName: (item) => item.getItemData()?.name || 'Unnamed',
+    isItemFolder: () => true,
+    dataLoader: {
+      getItem: (itemId) => {
+        const object = scene.current.getObjectById(Number(itemId))
+        if (!object) throw Error(`No object exists with id ${itemId}`)
+        return object
+      },
+      getChildren: (itemId) => {
+        const object = scene.current.getObjectById(Number(itemId))
+        if (!object) throw Error(`No object exists with id ${itemId}`)
+        const childIds: string[] = []
+        for (const child of object.children) {
+          childIds.push(child.id.toString())
+        }
+        return childIds
+      },
+    },
+    features: [syncDataLoaderFeature, selectionFeature, hotkeysCoreFeature],
+  })
 
-function TreeNode({ node }: { node: TreeItem }) {
-  return (
-    <li>
-      {node.name}
-      {node.children.length > 0 && (
-        <ul className="pl-3">
-          {node.children.map((child, i) => (
-            <TreeNode key={i} node={child} />
-          ))}
-        </ul>
-      )}
-    </li>
-  )
-}
-
-export default function Outline() {
-  const { currentObject } = useEditor()
-
-  if (!currentObject) return null
-
-  const tree = getObjectTree(currentObject)
+  useEffect(() => {
+    tree.rebuildTree()
+  }, [objectVersion, currentObject])
 
   return (
-    <>
-      <div className="flex w-full border-0 border-b border-zinc-700">
+    <div
+      {...tree.getContainerProps()}
+      className="flex flex-col items-start"
+    >
+      <div className='border-b border-zinc-700 w-full'>
         <p>Outline</p>
       </div>
-      <ul className="w-full h-full">
-        <TreeNode node={tree} />
-      </ul>
-    </>
+      {tree.getItems().map((item) => (
+        <button
+          {...item.getProps()}
+          key={item.getId()}
+          style={{ paddingLeft: `${item.getItemMeta().level * 20}px` }}
+        >
+          <div
+            className={clsx({
+              focused: item.isFocused(),
+              expanded: item.isExpanded(),
+              selected: item.isSelected(),
+              folder: item.isFolder(),
+            })}
+          >
+            {item.getItemName()}
+          </div>
+        </button>
+      ))}
+    </div>
   )
 }
