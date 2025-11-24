33c7bace37516b54e326d53970eb85f1434e4198
heliacer <heliacer@gmx.ch>
2025-11-24 20:12:06 +0100

    peak property panel :>

--- app/editor/components/panels/PropertiesPanel.tsx
+++ app/editor/components/panels/PropertiesPanel.tsx
@@ -1,58 +1,67 @@
 import { useEditor } from '../../lib/EditorContext'
 import { useState } from 'react'
 import ObjectProps from '../ui/properties/object'
-import { Cone, Cuboid, Wrench } from 'lucide-react'
-import clsx from 'clsx'
+import { Cone, Cuboid, LucideIcon, Wrench } from 'lucide-react'
+import clsx, { ClassValue } from 'clsx'
 import GeometryProps from '../ui/properties/geometry'
 import MaterialProps from '../ui/properties/material'
 
 type PropertiesTab = 'object' | 'geometry' | 'material'
 
-/** @todo update: this is actually starting to get peak, need to have dynamic props for each object tho */
+interface PropertyTabButtonProps {
+  Icon: LucideIcon
+  className?: string
+  active: boolean
+  setActive: () => void
+}
+
+function PropertyTabButton({ Icon, className, active, setActive }: PropertyTabButtonProps) {
+  return (
+    <button
+      onClick={setActive}
+      className={clsx(
+        'border-l rounded-md cursor-pointer',
+        active
+          ? 'border-teal-600 bg-zinc-800'
+          : 'border-transparent hover:bg-zinc-800 hover:border-zinc-700',
+        className
+      )}
+    >
+      <div className={clsx(
+        'border border-l-0 p-0.5 rounded-md',
+        active ? 'border-zinc-700' : 'border-transparent hover:border-zinc-700'
+      )}>
+        <Icon size={14} />
+      </div>
+    </button>
+  )
+}
+
 export default function PropertiesPanel() {
   const [activeTab, setActiveTab] = useState<PropertiesTab>('object')
-
   const { currentObject } = useEditor()
-
   if (!currentObject) return
 
   return (
     <div className="flex h-full">
-      <nav className="flex flex-col gap-1 border-r border-zinc-700 p-0.5">
-        {/* getting nasty, needs refactor */}
-        <button
-          onClick={() => setActiveTab('object')}
-          className={clsx(
-            'border p-0.5 rounded-md cursor-pointer',
-            activeTab === 'object'
-              ? 'bg-zinc-750 border-zinc-700'
-              : 'bg-zinc-800 border-zinc-700 hover:bg-zinc-750'
-          )}
-        >
-          <Wrench size={14} />
-        </button>
-        <button
-          onClick={() => setActiveTab('geometry')}
-          className={clsx(
-            'border p-0.5 rounded-md cursor-pointer',
-            activeTab === 'geometry'
-              ? 'bg-zinc-750 border-zinc-700'
-              : 'bg-zinc-800 border-zinc-700 hover:bg-zinc-750'
-          )}
-        >
-          <Cone className="text-blue-300" size={14} />
-        </button>
-        <button
-          onClick={() => setActiveTab('material')}
-          className={clsx(
-            'border p-0.5 rounded-md cursor-pointer',
-            activeTab === 'material'
-              ? 'bg-zinc-750 border-zinc-700'
-              : 'bg-zinc-800 border-zinc-700 hover:bg-zinc-750'
-          )}
-        >
-          <Cuboid className="text-teal-300" size={14} />
-        </button>
+      <nav className="flex flex-col gap-1 p-0.5">
+        <PropertyTabButton
+          Icon={Wrench}
+          active={activeTab === 'object'}
+          setActive={() => setActiveTab('object')}
+        />
+        <PropertyTabButton
+          Icon={Cone}
+          className='text-blue-300'
+          active={activeTab === 'geometry'}
+          setActive={() => setActiveTab('geometry')}
+        />
+        <PropertyTabButton
+          Icon={Cuboid}
+          className='text-teal-300'
+          active={activeTab === 'material'}
+          setActive={() => setActiveTab('material')}
+        />
       </nav>
       <div className="p-1.5 flex flex-col gap-2 text-xs">
         {activeTab === 'object' && <ObjectProps object={currentObject} />}
