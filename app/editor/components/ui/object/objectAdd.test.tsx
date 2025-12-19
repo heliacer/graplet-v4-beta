3faf9e51ffc58dcb06ba585e145163ba2411a2bf
heliacer <heliacer@gmx.ch>
2025-12-19 17:19:43 +0100

    finish new dropdown, impl prog

--- app/editor/components/ui/object/objectAdd.test.tsx
+++ app/editor/components/ui/object/objectAdd.test.tsx
@@ -1,9 +1,41 @@
 import { useObjectActions } from '@/app/editor/lib/hooks/useObjectActions'
+import { SGeometryT } from '@/app/editor/lib/types'
 import { Dropdown, DropdownItemProps } from '@/app/ui/components/Dropdown.test'
-import { Box, Component, DiamondPlus, Star, Wrench } from 'lucide-react'
+import { Box, Camera, Component, DiamondPlus, Lightbulb } from 'lucide-react'
+
+const geometries: SGeometryT[] = [
+  'BoxGeometry',
+  'CircleGeometry',
+  'ConeGeometry',
+  'CylinderGeometry',
+  'DodecahedronGeometry',
+  'IcosahedronGeometry',
+  'OctahedronGeometry',
+  'PlaneGeometry',
+  'RingGeometry',
+  'SphereGeometry',
+  'TetrahedronGeometry',
+  'TorusGeometry',
+  'TorusKnotGeometry'
+]
 
 export function ObjectAdd() {
   const { addObject } = useObjectActions()
+
+  const meshChildren: DropdownItemProps[] = geometries.map((geo) => ({
+    label: geo.slice(0, -8),
+    onClick: () =>
+      addObject({
+        name: geo.slice(0, -8),
+        type: 'Mesh',
+        geometry: { type: geo, args: [] },
+        material: { type: 'MeshStandardMaterial' }
+      })
+  }))
+
+  const lightChildren: DropdownItemProps[] = []
+
+  const cameraChildren: DropdownItemProps[] = []
 
   const items: DropdownItemProps[] = [
     {
@@ -14,39 +46,17 @@ export function ObjectAdd() {
     {
       label: 'Mesh',
       Icon: Box,
-      children: [
-        {
-          label: 'Box',
-          onClick: () => addObject({
-            name: 'Cube',
-            type: 'Mesh',
-            geometry: { type: 'BoxGeometry', args: [] },
-            material: { type: 'MeshStandardMaterial' }
-          })
-        },
-        {
-          label: 'More',
-          Icon: Wrench,
-          children: [
-            {
-              label: 'Test',
-            },
-            {
-              label: '???',
-              children: [
-                {
-                  label: 'may26',
-                }
-              ]
-            }
-          ]
-        }
-        /** @todo add map of all geometries, maybe add icon */
-      ]
+      children: meshChildren
     },
     {
-      label: 'Action',
-      Icon: Star
+      label: 'Light',
+      Icon: Lightbulb,
+      children: lightChildren
+    },
+    {
+      label: 'Camera',
+      Icon: Camera,
+      children: cameraChildren
     }
   ]
 
--- app/ui/components/Dropdown.test.tsx
+++ app/ui/components/Dropdown.test.tsx
@@ -25,9 +25,8 @@ export interface DropdownProps {
 }
 
 function DropdownItemList({ items }: { items: DropdownItemProps[] }) {
-  /** @todo pass {i} so that the child dropdown knows where to allocate, using spacing of i*child height */
   return (
-    <ul>
+    <ul className='text-xs bg-zinc-800 border border-zinc-700 rounded py-0.5'>
       {items.map((item, i) => (
         <DropdownItem key={i} {...item} />
       ))}
@@ -41,23 +40,30 @@ function DropdownItem({ label, Icon, onClick, children }: DropdownItemProps) {
 
   return (
     <li
+      className='relative'
       onMouseEnter={() => setActive(true)}
       onMouseLeave={() => setActive(false)}
     >
-      <button
-        className='flex gap-1 items-center'
-        onClick={(e) => {
-          onClick?.(e)
-          if (!children) {
-            setIsOpen(false)
-          }
-        }}
-      >
-        {Icon && <Icon size={14} />}
-        {label}
-        {children && <ChevronRight size={14} />}
-      </button>
-      <div className='absolute left-full top-1/2'>
+      <div className='mx-0.5'>
+        <button
+          className={clsx(
+            'flex gap-1 px-0.5 items-center w-full',
+            'rounded border border-transparent',
+            'hover:bg-zinc-700 hover:border-zinc-600',
+          )}
+          onClick={(e) => {
+            onClick?.(e)
+            if (!children) {
+              setIsOpen(false)
+            }
+          }}
+        >
+          {Icon && <Icon size={14} />}
+          {label}
+          {children && <ChevronRight size={14} />}
+        </button>
+      </div>
+      <div className='absolute left-full top-0'>
         {children && active && <DropdownItemList items={children} />}
       </div>
     </li>
@@ -79,11 +85,11 @@ export function Dropdown({ label, items, Icon }: DropdownProps) {
 
   return (
     <DropdownContext.Provider value={{ isOpen, setIsOpen }}>
-      <div ref={refClick} className='text-sm relative'>
+      <div ref={refClick}>
         <button
           onClick={() => setIsOpen(prev => !prev)}
           className={clsx(
-            'flex items-center gap-1 px-1',
+            'text-sm flex items-center gap-1 px-1 mb-0.5',
             'border rounded-md',
             'border-zinc-700',
             'hover:bg-zinc-750 bg-zinc-800'
