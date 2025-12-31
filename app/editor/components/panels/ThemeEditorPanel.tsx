fe8fb9698b5d8eb6a7e17a9ea493aff76b4e152f
heliacer <heliacer@gmx.ch>
2025-12-31 12:33:31 +0100

    impr: theme editor

--- app/editor/components/panels/ThemeEditorPanel.tsx
+++ app/editor/components/panels/ThemeEditorPanel.tsx
@@ -13,46 +13,60 @@ function ColorPicker({
   const { currentTheme } = useEditor()
   const variable = `--${colorName}`
   const [isChanged, setIsChanged] = useState(false)
-  const [colorValue, setColorValue] = useState(() => {
+  const [colorValue, setColorValue] = useState<string>(() => {
     const c = getComputedStyle(document.documentElement)
       .getPropertyValue(variable)
       .trim()
-    return formatHex(c)
+    const hex = formatHex(c)
+    if (!hex) throw Error(`unable to format ${c} from ${variable}`)
+    return hex
   })
 
+  const [debouncedColor, setDebouncedColor] = useState(colorValue)
+
   useEffect(() => {
-    const cssColor = getComputedStyle(document.documentElement)
+    const c = getComputedStyle(document.documentElement)
       .getPropertyValue(variable)
       .trim()
-    setColorValue(formatHex(cssColor))
+    const hex = formatHex(c)
+    if (!hex) throw Error(`unable to format ${c} from ${variable}`)
+    setColorValue(hex)
     setIsChanged(false)
   }, [currentTheme, resetVersion, variable])
 
+  useEffect(() => {
+    const handle = setTimeout(() => {
+      document.documentElement.style.setProperty(variable, debouncedColor)
+    }, 5)
+    return () => clearTimeout(handle)
+  }, [debouncedColor, variable])
+
   return (
-    <div
+    <label
+      htmlFor={colorName}
       className={clsx(
-        'flex border px-0.5 rounded',
+        'cursor-pointer flex text-sm border px-0.5 rounded-md',
+        'hover:bg-ui-800',
         isChanged ? 'border-teal' : 'border-ui-700'
       )}
     >
       <input
         id={colorName}
-        className='w-6 h-6 cursor-pointer'
+        className='w-5 h-5 cursor-pointer'
         type='color'
         value={colorValue}
-        onChange={(e) => {
-          setIsChanged(true)
+        onChange={e => {
           const newColor = e.target.value
           setColorValue(newColor)
-          document.documentElement.style.setProperty(variable, newColor)
+          setDebouncedColor(newColor)
+          setIsChanged(true)
         }}
       />
-      <label htmlFor={colorName} className='cursor-pointer'>
-        {colorName}
-      </label>
-    </div>
+      {colorName}
+    </label>
   )
 }
+
 
 /**
  * @todo Export as CSS -> ready to implement
@@ -77,43 +91,51 @@ export default function ThemeEditorPanel() {
   ]
 
   return (
-    <div className='flex gap-4 m-2'>
-      <div className='flex flex-col gap-0.5'>
-        <p>UI Colors</p>
-        {[...Array(19)].map((_, key) => (
-          <ColorPicker
-            key={key}
-            colorName={`ui-${(key + 1) * 50}`}
-            resetVersion={resetVersion}
-          />
-        ))}
+    <div className='flex h-full'>
+
+      <div className='flex p-2 gap-4 h-full border-r border-ui-700'>
+        <div className='flex flex-col gap-1'>
+          <p>UI Colors</p>
+          {[...Array(19)].map((_, key) => (
+            <ColorPicker
+              key={key}
+              colorName={`ui-${(key + 1) * 50}`}
+              resetVersion={resetVersion}
+            />
+          ))}
+        </div>
+        <div className='flex flex-col gap-1'>
+          <p>Accent Colors</p>
+          {accentColors.map((accentColor, key) => (
+            <ColorPicker
+              key={key}
+              colorName={accentColor}
+              resetVersion={resetVersion}
+            />
+          ))}
+        </div>
       </div>
-      <div className='flex flex-col gap-0.5'>
-        <p>Accent Colors</p>
-        {accentColors.map((accentColor, key) => (
-          <ColorPicker
-            key={key}
-            colorName={accentColor}
-            resetVersion={resetVersion}
-          />
-        ))}
-      </div>
-      <div>
-        <p>Base Theme: {currentTheme}</p>
-        <button
-          className={clsx(
-            'text-sm flex gap-1 px-1 items-center',
-            'border rounded-md',
-            'border-ui-700',
-            'hover:bg-ui-750 bg-ui-800'
-          )}
-          onClick={() => {
-            document.documentElement.removeAttribute('style')
-            setResetVersion((v) => v + 1)
-          }}
-        >
-          Reset Colors
-        </button>
+
+
+      <div className='p-2 flex flex-col gap-1'>
+        <div className='flex flex-col gap-1'>
+          {/** @todo make this a "theme" switcher aswell */}
+          <p>base: {currentTheme}</p>
+          <button
+            className={clsx(
+              'text-sm flex gap-1 px-1 items-center',
+              'border rounded-md',
+              'border-ui-700',
+              'hover:bg-ui-750 bg-ui-800'
+            )}
+            onClick={() => {
+              document.documentElement.removeAttribute('style')
+              setResetVersion((v) => v + 1)
+            }}
+          >
+            Reset Colors
+          </button>
+        </div>
       </div>
     </div>
   )
