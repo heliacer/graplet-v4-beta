f0062d98cdcbab2fdacc15a7fe54edbbe30e3289
heliacer <heliacer@gmx.ch>
2026-01-06 19:12:43 +0100

    update np and pg adapter for prisma client v7

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,8 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 import { UserT } from './types'
+import { PrismaPg } from '@prisma/adapter-pg'
 import { genSalt, hash } from 'bcrypt'
 
-const prisma = new PrismaClient()
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+const prisma = new PrismaClient({ adapter })
+
 const saltRounds = 10
 
 export async function getUserByEmail(email: string) {
