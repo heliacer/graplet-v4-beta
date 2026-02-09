4fa96004af71d79425fd5167446d5964f018a3ad
heliacer <heliacer@gmx.ch>
2026-02-09 17:39:57 +0100

    make shortcuts a hook

--- app/editor/components/ui/footer.tsx
+++ app/editor/components/ui/footer.tsx
@@ -1,54 +1,7 @@
-import { useEffect } from 'react'
-import { useEditor } from '../../lib/EditorContext'
-
-const modifiers = [
-  'ShiftLeft',
-  'ShiftRight',
-  'ControlLeft',
-  'ControlRight',
-  'AltLeft',
-  'AltRight',
-  'MetaLeft',
-  'MetaRight'
-]
-
-/* 
-function chordToDisplay(chord: string) {
-  // used for later, in the keybind tab when it get's the keybind map (with the keybind chord -> binded to the command action)
-}
- */
+import { useShortcuts } from '../../lib/hooks/useShortcuts'
 
 export function Footer() {
-  const { dvApi } = useEditor()
-
-  useEffect(() => {
-    if (!dvApi) return
-
-    const handleKeydown = (e: KeyboardEvent) => {
-      if (modifiers.includes(e.code)) return
-
-      const parts: string[] = []
-      if (e.ctrlKey) {
-        parts.push('ctrl')
-      }
-      if (e.shiftKey) {
-        parts.push('shift')
-      }
-      if (e.altKey) {
-        parts.push('alt')
-      }
-      parts.push(e.code)
-      const chord = parts.join('+')
-      // const display = chordToDisplay(chord)
-      console.log('action with', chord)
-    }
-
-    document.addEventListener('keydown', handleKeydown)
-
-    return () => {
-      document.removeEventListener('keydown', handleKeydown)
-    }
-  }, [dvApi])
+  useShortcuts()
 
   return (
     <footer className='h-6 flex items-center'>
---
+++ app/editor/lib/hooks/useShortcuts.ts
@@ -0,0 +1,54 @@
+import { useEffect } from 'react'
+import { useEditor } from '../EditorContext'
+
+/**
+ * @todo display (e.g / instead of Shift+Key7 and a instead of Keya )
+ */
+function chordToDisplay(chord: string) {
+  return chord + '(not implemented)'
+}
+
+const modifiers = [
+  'ShiftLeft',
+  'ShiftRight',
+  'ControlLeft',
+  'ControlRight',
+  'AltLeft',
+  'AltRight',
+  'MetaLeft',
+  'MetaRight'
+]
+
+export function useShortcuts() {
+  const { dvApi } = useEditor()
+
+  useEffect(() => {
+    if (!dvApi) return
+
+    const handleKeydown = (e: KeyboardEvent) => {
+      if (modifiers.includes(e.code)) return
+
+      const parts: string[] = []
+      if (e.ctrlKey) {
+        parts.push('ctrl')
+      }
+      if (e.shiftKey) {
+        parts.push('shift')
+      }
+      if (e.altKey) {
+        parts.push('alt')
+      }
+      parts.push(e.code)
+      const chord = parts.join('+')
+      const display = chordToDisplay(chord)
+      console.log('chord as display: ', display)
+      console.log('action with', chord)
+    }
+
+    document.addEventListener('keydown', handleKeydown)
+
+    return () => {
+      document.removeEventListener('keydown', handleKeydown)
+    }
+  }, [dvApi])
+}
